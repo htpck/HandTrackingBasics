b337c9f2d5b8ad72bc9d0a9e7285c02e9bd5357f
Hilal <hayhilal@gmail.com>
2022-04-05 17:14:27 +0300

    InitialCommit

---
+++ Assets/LogScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogScript : MonoBehaviour
+{
+    public void OnThumsUpLeft()
+    {
+        Debug.Log("ThumbsUpLeft");
+    }
+    public void OnThumsDownLeft()
+    {
+        Debug.Log("ThumbsDownLeft");
+    }
+}
